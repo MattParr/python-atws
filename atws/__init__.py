5e8e0e63e77d1ee89ac6ce8fed1245785aa0b174
matt <matt@parr.geek.nz>
2015-10-20 17:10:50 +0100

    added todo: picklist object for programatic picklist conversion between friendly names and ids.

--- atws/__init__.py
+++ atws/__init__.py
@@ -1,3 +1,15 @@
+'''
+@todo: create a picklist object that allows:
+preferrably have the wrapper auto generate these objects if needed
+but also allow them to be passed in (from a pickle cache later on)
+tp = PickList('Ticket')
+tp.Status(5) 
+'Complete'
+tp.Status('Complete')
+5
+tp.IssueType('ThisIssue')
+23455
+'''
 from pytz import timezone
 from wrapper import connect
 from query import Query
