72f4e3bde6b01fe3bd0ef2ae1062d9b3d0287182
matt <matt@parr.geek.nz>
2015-10-27 20:27:18 +0000

    fixes TypeError expected 1 argument got 0 because self not passed to parent method

--- atws/monkeypatch/userdefinedfields.py
+++ atws/monkeypatch/userdefinedfields.py
@@ -25,7 +25,7 @@ def mp_getattr(entity,attr):
             return helpers.get_udf_value(entity._wrapper,entity,attr)
         except AttributeError:
             raise AttributeError( 'no attribute or udf named {}'.format(attr) )
-    return sudsobject.__getattribute__(attr)
+    return sudsobject.__getattribute__(self,attr)
 
 
 def mp_setattr(self,name,value):
