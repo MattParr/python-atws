008f129207e6cb6cdd07af4582a1de75ea680a07
matt <matt@parr.geek.nz>
2016-04-29 12:23:50 +0100

    version increment: fixed #11 features: can always bulk update regardless of how many udfs in an entity

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'atws',
   scripts=['src/create_picklist_module.py'],
   packages = ['atws','atws.monkeypatch'],
-  version = '0.1.5',
+  version = '0.1.6',
   install_requires=[
         'requests',
         'pytz',
