3f3edcb8b5bfeba76a441a24989c996909341768
matt <matt@biglaptop.home>
2016-06-29 20:39:54 +0100

    convert to click

--- atws/create_picklist_module.py
+++ atws/create_picklist_module.py
@@ -1,41 +1,34 @@
 # -*- coding: utf-8 -*-
-import click
 import os
-import argparse
-import atws
-from atws.helpers import create_atvar_module
-from atws.monkeypatch.duckpunch import BLACKLISTED_TYPES, get_api_types
+import click
+from wrapper import connect
+from helpers import create_atvar_module
+from monkeypatch.duckpunch import BLACKLISTED_TYPES, get_api_types
 
 @click.command()
-def main(args=None):
-    parser = argparse.ArgumentParser(description='test')
-    parser.add_argument('target_path')
-    parser.add_argument('--username', 
-                        default=os.environ.get('AUTOTASK_API_USERNAME', None))
-    parser.add_argument('--password', 
-                        default=os.environ.get('AUTOTASK_API_PASSWORD', None))
-    parser.add_argument('--url', 
-                        default=os.environ.get('AUTOTASK_API_URL', None))
-    parser.add_argument('--entities',
-                        help='Space delimited entity types for inclusion',
-                        nargs='+', 
-                        default=[]
-                        )
+@click.argument('target_path')
+@click.option('--username', 
+              default=os.environ.get('AUTOTASK_API_USERNAME', None), 
+              help='your Autotask username')
+@click.option('--password',
+              default=os.environ.get('AUTOTASK_API_PASSWORD', None), 
+              help='your Autotask password')
+@click.option('--url',
+              default=os.environ.get('AUTOTASK_API_URL', None), 
+              help='your Autotask webservices URL (wsdl file)')
+@click.option('--entities',
+              default=list(), 
+              help='Space delimited entity types for inclusion')
+def main(target_path, username, password, url, entities):
     
-    args = parser.parse_args()
-    if not args.username or not args.password:
-        exit(parser.print_usage())
+    at = connect(url=url,
+                      username=username,
+                      password=password)
     
-    at = atws.connect(url=args.url,
-                      username=args.username,
-                      password=args.password)
-    
-    if args.entities:
-        entities = args.entities
-    else:
+    if not entities:
         entities = get_api_types(at.client, BLACKLISTED_TYPES)
     
-    create_atvar_module(at, entities, args.target_path)
+    create_atvar_module(at, entities, target_path)
 
 
 if __name__ == "__main__":
