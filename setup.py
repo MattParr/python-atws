ba8594c687d89de06d536897daca71c9a60b75c5
Matt Parr <matt@parr.geek.nz>
2016-07-06 12:53:35 +0100

    Bump version: 0.2.0 → 0.3.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ test_requirements = [
 
 setup(
     name='atws',
-    version='0.2.0',
+    version='0.3.0',
     description="python-atws is a wrapper for the AutoTask SOAP webservices API",
     long_description=readme + '\n\n' + history,
     author="Matt Parr",
