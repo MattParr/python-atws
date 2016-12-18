f137ef453e02abba872e25d7d5012eb04c5bfe08
Matt Parr <matt@parr.geek.nz>
2016-12-18 22:12:52 +0000

    added logging to picklist creation module
    added an option “log_level” to the create_picklist_module script to
    enable specifying logging level

--- atws/create_picklist_module.py
+++ atws/create_picklist_module.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 import os
+import logging
 import click
 from .wrapper import connect
 from .helpers import create_atvar_module
@@ -20,12 +21,13 @@ from .monkeypatch.duckpunch import BLACKLISTED_TYPES, get_api_types
 @click.option('--entities',
               default=list(), 
               help='Space delimited entity types for inclusion')
-def main(target_path, username, password, url, entities):
-    
-    at = connect(url=url,
-                      username=username,
-                      password=password)
-    
+@click.option('--log_level',
+              default=os.environ.get('LOG_LEVEL', None),
+              help='python logging root logger level with basic config')
+def main(target_path, username, password, url, entities, log_level):
+    if log_level:
+        logging.basicConfig(level=getattr(logging,log_level))
+    at = connect(url=url, username=username, password=password)
     if not entities:
         entities = get_api_types(at.client, BLACKLISTED_TYPES)
     
