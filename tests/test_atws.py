a6a89a701a8d02862f80afeb8c38b6830ac15197
Matt Parr <matt@parr.geek.nz>
2018-01-26 23:12:48 +0000

    removed tests for create_picklist_module

--- tests/test_atws.py
+++ tests/test_atws.py
@@ -12,9 +12,7 @@ Tests for `atws` module.
 import sys
 import unittest
 from contextlib import contextmanager
-from click.testing import CliRunner
 import atws
-from atws import create_picklist_module
 
 query_test_output=u'<?xml version="1.0" ?>\n<queryxml>\n\t<entity>Ticket</entity>\n\t<query>\n\t\t<field>\n\t\t\tStatus\n\t\t\t<expression op="NotEqual">5</expression>\n\t\t</field>\n\t\t<condition>\n\t\t\t<condition operator="OR">\n\t\t\t\t<field>\n\t\t\t\t\tIssueType\n\t\t\t\t\t<expression op="GreaterThan">345</expression>\n\t\t\t\t</field>\n\t\t\t</condition>\n\t\t</condition>\n\t</query>\n</queryxml>\n'
 
@@ -42,16 +40,6 @@ class TestAtws(unittest.TestCase):
         query_output = query.pretty_print()
         assert repr(query_test_output) == repr(query_output)
         
-        
-    def test_command_line_interface(self):
-        runner = CliRunner()
-        #         result = runner.invoke(create_picklist_module.main)
-        #         assert result.exit_code == 0
-        #         assert 'atws.create_picklist_module.main' in result.output
-        help_result = runner.invoke(create_picklist_module.main, ['--help'])
-        assert help_result.exit_code == 0
-        assert 'Show this message and exit.' in help_result.output
-
 
 if __name__ == '__main__':
     sys.exit(unittest.main())
